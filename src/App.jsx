511398620baa9ec3dbfdce4c0cd18ac1001e4d86
marketinganalystac <marketing.analyst.ac@gmail.com>
2026-08-25 09:42:32 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ function LoginScreen() {
         <div className="ac-login-wrap">
           <video
             className="ac-login-video"
-            src="hhttps://cdn.jsdelivr.net/gh/marketinganalystac/CatalogoRepuestos-admin@HEAD/public/mechanic.webm"
+            src="https://cdn.jsdelivr.net/gh/marketinganalystac/CatalogoRepuestos-admin@HEAD/public/mechanic.webm"
             autoPlay muted loop playsInline
           />
         </div>
